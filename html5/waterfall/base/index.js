839a72fbfdd2540f744e60f43e77fef575c4d202
wxc4234 <2735229038@qq.com>
2023-10-25 19:13:34 +0800

    瀑布流布局

---
+++ html5/waterfall/base/index.js
@@ -0,0 +1,29 @@
+var arr=['a','b','c','d']
+var arr2=[1,2,3]
+// arr.push('e')尾部添加元素
+// arr.pop(1)尾部删除
+// arr.unshift(1)头部添加元素
+// arr.shift()头部删除
+// arr.splice(1,1) //从这个点开始删除 删除几个元素
+//arr.splice(1,0,true)//添加元素
+// for(var i=0;i<arr.length;i++){
+//     arr[i]=arr[i]+1
+// }
+
+// arr.forEach(function(item,index,arr){ //回调函数    元素，下标，元素组
+// console.log(item,index,arr);
+// })
+    //console.log(arr[i])
+
+// var newArr=arr.map(function(item,index,arr){ //map可以返回一个新数组
+// return item+2
+// })
+
+// console.log(arr.indexOf('c'，2));查找某个元素，返回其下标 可以接收第二个元素，从下标为2开始找
+// console.log(arr.lastIndexOf('b'));从右往左数
+
+// console.log(arr.concat(arr2));
+// console.log([].concat(arr,arr2));
+//拼接arr和arr2
+
+ 
