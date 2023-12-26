0f398b3fce958b080ca95a1090439fd46476705d
wxc4234 <2735229038@qq.com>
2023-12-26 21:29:28 +0800

    评分

---
+++ vue/rate/src/App.vue
@@ -0,0 +1,31 @@
+
+<script setup>
+
+// 响应式思想
+// vue3 语法 ref 代替了 vue2 的 data，就表示响应式
+import { ref } from 'vue'
+// 组件化思想
+import Rate from './components/Rate.vue'
+// vue 主要工作开发组件
+let score = ref(3) //data
+setTimeout(() => {
+  // 会发生状态的改变
+  // 组件 = UI + 数据状态
+  score.value = 4  // 页面会自动更新
+}, 2000)
+</script>
+
+
+<template>
+  <div>
+    <!-- 评分组件, 5颗星星, 点亮 组件思维-->
+  </div>
+  <Rate :value="score"/>
+  <Rate :value="score" theme="red"/>
+  <!-- theme是属性，red是属性值 -->
+  <!-- 动态数据绑定 -->
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue/rate/src/components/Rate.vue
@@ -0,0 +1,27 @@
+<template>
+  <div :style="fontStyle">
+    Rate {{ rate }}
+    <!-- value是父组件传递过来的值，rate是计算属性 -->
+  </div>
+</template>
+
+<script setup>
+import { defineProps ,computed} from 'vue' //defineProps 定义属性, computed 计算属性
+// 父子组件之间的信任通信
+// 外界传输的参数对象
+let props=defineProps({
+  value: Number,
+  // 对参数的校验要求比较复杂的时候，可以使用函数
+  theme: {
+    type: String,
+    default: 'orange'
+  }
+})
+// 计算属性，data/props 再计算一下
+let rate = computed(() => "★★★★★☆☆☆☆☆".slice(5 - props.value, 10 - props.value))
+let fontStyle = computed(() => 'color:${props.theme};')
+</script>
+
+<style scoped>
+
+</style>
