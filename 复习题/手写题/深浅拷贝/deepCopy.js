038339f5556e33a2a2e6694b53370e0aef00fdcb
wxc4234 <2735229038@qq.com>
2024-04-26 22:56:58 +0800

    手写深浅拷贝

--- 复习题/手写题/深浅拷贝/deepCopy.js
+++ 复习题/手写题/深浅拷贝/deepCopy.js
@@ -6,25 +6,66 @@ let obj = {
 }
 
 // 1. 可以使用递归的方法实现深拷贝
+// function deepCopy(obj) {
+//   let newObj = {};
+//   for (let key in obj) {
+//     if (obj[key] instanceof Array) {
+//       newObj[key] = [];
+//       for (let j = 0; j < obj[key].length; j++){
+//         newObj[key][j] = obj[key][j];
+//       }
+//     } else if (typeof obj[key] === 'object') {
+//       newObj[key] = deepCopy(obj[key]);
+//     } else {
+//       newObj[key] = obj[key];
+//     }
+//   }
+
+//   return newObj;
+// }
+
+// 2.可以通过Json.parse(Json.stringify())实现
+// function deepCopy(obj) {
+//   let newObj = {};
+
+//   newObj = JSON.parse(JSON.stringify(obj));
+
+//   return newObj;
+// }
+
+// 3.可以通过MessageChannel实现
+
+// function deepCopy(obj) {
+//   return new Promise((resolve)=> {
+//     let { port1, port2 } = new MessageChannel();
+//     port1.postMessage(obj);
+//     port2.onmessage = (msg) => {
+//       resolve(msg.data)
+//     }
+//   })
+// }
+
+// let newObj = null;
+// deepCopy(obj).then((res) => {
+//   newObj = res;
+//   obj.b.c = 3;
+//   console.log(newObj);
+// })
+
+// obj.b.c = 3;
+// console.log(newObj);
+
+// 6.可以通过structuredClone来实现
+
 function deepCopy(obj) {
   let newObj = {};
-  for (let key in obj) {
-    if (obj[key] instanceof Array) {
-      newObj[key] = [];
-      for (let j = 0; j < obj[key].length; j++){
-        newObj[key][j] = obj[key][j];
-      }
-    } else if (typeof obj[key] === 'object') {
-      newObj[key] = deepCopy(obj[key]);
-    } else {
-      newObj[key] = obj[key];
-    }
-  }
-
-  return newObj;
+  newObj = structuredClone(obj);
+  return newObj
 }
 
-let newObj = deepCopy(obj)
+let newObj = {};
+newObj = deepCopy(obj);
 
 obj.b.c = 3;
+
 console.log(newObj);
